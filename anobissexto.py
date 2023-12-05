98f948f6ebf4c6b71ea75201aa4dc8351a310af1
Alysson Silveira <silveira.alysson@gmail.com>
2023-12-05 15:38:08 -0300

    -- terceiro commit Alysson

--- anobissexto.py
+++ anobissexto.py
@@ -1,10 +1,13 @@
-# aula 6 de Python Turma 21 - 04/12/2023
+# Exercício 6 de Python - Senai Turma 21
 # programa para calcular se um ano é bissexto ou não
 
 # Apresentação
-print ('O ano bissexto acontece a cada quatro anos e têm duração de 366 dias. A inclusão de um dia foi feita para aproximar '
-      'o calendário ao movimento de translação da Terra \n(365 dias, 5 horas, 48 minutos e 46 segundos. '
-      '\nEssas horas que ultrapassam os 365 dias são compensadas a cada quatro anos, no dia 29 de fevereiro')
+print ('\nO ano bissexto acontece a cada quatro anos e têm duração de 366 dias. '
+       '\nA inclusão de um dia foi feita para aproximar '
+      'o calendário ao movimento de translação da Terra: '
+       '\n365 dias, 5 horas, 48 minutos e 46 segundos. '
+      '\nPor isso que essas horas que ultrapassam os 365 dias são compensadas '
+       'a cada quatro anos, no dia 29 de fevereiro')
 def bissexto(ano):
     # SE o ano for divisível por 4 *E* também *Não* for divisível por 100.
     # Porque anos divisíveis por 100 só são bissextos quando também são divisíveis por 400.
