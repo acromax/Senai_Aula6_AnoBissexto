467ec08e45e4fded8baecc94b0eebe633ac7550e
Alysson <silveira.alysson@gmail.com>
2023-12-05 12:13:15 -0300

    - segundo commit Alysson

--- anobissexto.py
+++ anobissexto.py
@@ -1,21 +1,23 @@
-# "O ano bissexto acontece a cada quatro anos e tem duração de 366 dias, diferentemente dos demais que têm 365 dias.
-# A inclusão de um dia foi feita para aproximar o calendário ao movimento de translação da Terra, tempo que o planeta leva para dar a volta no Sol,
-# que é de 365 dias, 5 horas, 48 minutos e 46 segundos. Essas horas que ultrapassam os 365 dias são compensadas a cada quatro anos, no dia 29 de fevereiro."
+# aula 6 de Python Turma 21 - 04/12/2023
+# programa para calcular se um ano é bissexto ou não
 
+# Apresentação
+print ('O ano bissexto acontece a cada quatro anos e têm duração de 366 dias. A inclusão de um dia foi feita para aproximar '
+      'o calendário ao movimento de translação da Terra \n(365 dias, 5 horas, 48 minutos e 46 segundos. '
+      '\nEssas horas que ultrapassam os 365 dias são compensadas a cada quatro anos, no dia 29 de fevereiro')
 def bissexto(ano):
-    # SE o ano quando for divisível por 4 E também não deixa resto quando divisível por 100.
-    # Anos divisíveis por 100 só são bissextos quando também sejam divisíveis por 400.
+    # SE o ano for divisível por 4 *E* também *Não* for divisível por 100.
+    # Porque anos divisíveis por 100 só são bissextos quando também são divisíveis por 400.
     if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-
         return True
     else:
         return False
 
 # Entrada
-ano = int(input("Digite um ano: "))
+ano = int(input("\nDigite um ano: "))
 
 # Saída
 if bissexto(ano):
-    print(f"{ano} é um ano bissexto.")
+    print(f'\n{ano} é um ano bissexto.')
 else:
-    print(f"{ano} não é um ano bissexto.")
+    print(f'\n{ano} não é um ano bissexto.')
